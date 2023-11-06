e0bac5cd5ad8f90c0f27a9c82a8afb218d9be862
Abhay Krishnan M N <75512915+abhaykrishnanmn@users.noreply.github.com>
2023-11-06 21:50:31 +0530

    Update Reverse.js

--- Reverse.js
+++ Reverse.js
@@ -22,20 +22,4 @@ function reverseString(str) {
 
 const inputSentence = "This is a sunny day";
 const reversedSentence = reverseWords(inputSentence);
-console.log(reversedSentence);B. Perform sorting of an array in descending order (using a simple sorting algorithm, e.g., bubble sort):function bubbleSortDescending(arr) {
-    let n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
-            if (arr[j] < arr[j + 1]) {
-                // Swap the elements
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
-const inputArray = [5, 2, 9, 1, 5, 6];
-bubbleSortDescending(inputArray);
-console.log(inputArray);
+console.log(reversedSentence);
